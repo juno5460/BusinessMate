4ffe5989d0cb7052d646e0816d25b951c412f7ad
Justin Yip <justin.yip@foxmail.com>
2013-05-29 15:37:55 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , contract = require('./routes/contract')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+//contracts
+app.get('/contracts', contract.list);
+app.get('/contracts/:id', contract.show);
+app.post('/contracts', contract.create);
+app.put('/contracts/:id', contract.update);
+// app.destroy('/contracts', contract.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/contract.js
@@ -0,0 +1,23 @@
+var mongodb = require('mongodb');
+/*
+ * GET contracts listing.
+ */
+exports.list = function(req, res){
+  res.send("respond with a resource");
+};
+
+exports.show = function(req, res){
+  res.send("respond with a resource");
+};
+
+exports.create = function(req, res){
+  res.send("respond with a resource");
+};
+
+exports.update = function(req, res){
+  res.send("respond with a resource");
+};
+
+exports.destroy = function(req, res){
+  res.send("respond with a resource");
+};
