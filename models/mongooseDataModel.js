daef76b9e9a50611a4de40adc2c94317dc8a4b2b
Daniel <caiyidong007@163.com>
2013-06-09 17:34:30 +0800

    调试接口
    调试通过create,index,show接口,update没通过进行不了put操作,destroy还没测试,

--- models/mongooseDataModel.js
+++ models/mongooseDataModel.js
@@ -5,22 +5,23 @@ var contractData1 = get.returnData1(); //获取数据源
 var contractData2 = get.returnData2();
 
 var db = mongoose.connection;
-var contractSchema = mongoose.Schema({
-	id: String,
-	businessName: String,
-	beginDate: String,
-	endDate: String,
-	events: Array,
-	state: String
+var contractSchema = mongoose.Schema({ //创建合同模型对象
+	cid: String, //合同的存储id
+	id: String, //合同编号
+	businessName: String, //合同名称
+	beginDate: String, //开始日期
+	endDate: String, //结束日期
+	events: Array, //合同事件
+	state: String //合同状态
 });
-var Contract = mongoose.model('Contract', contractSchema);
+var Contract = mongoose.model('Contract', contractSchema); //创建新合同对象,并关联到模型
 
 
 exports.connectDb = function() {
 	mongoose.connect('mongodb://localhost/contractDB');
 	db.on('error', console.error.bind(console, 'connection error:'));
 	db.once('open', function callback() {
-		console.log("helo");
+		console.log("hello database");
 	});
 };
 //增
@@ -39,12 +40,27 @@ exports.checkData = function(callback) {
 //改
 exports.updateData = function(callback) {
 	Contract.update({
-		id: 'CA123'
+		id: 'AC20130405'
 	}, {
-		businessname: 'ZZ商务合同'
+		"businessName": " 深交所架构改造",
+		"state": " 二次谈判",
+		"beginDate": "2013-06-10",
+		"endDate": "2015-01-01",
+		"events": [{
+				"remark": "事件32",
+				"date": "",
+				"title": "事件1",
+				"id": "3"
+			}, {
+				"remark": "事件11",
+				"date": "",
+				"title": "事件1",
+				"id": "3"
+			}
+		]
 	}, function() {
 		Contract.find({
-			id: 'CA123'
+			id: 'AC20130405'
 		}, function(err, docs) {
 			callback(docs);
 			db.close();
@@ -66,6 +82,7 @@ exports.removeData = function(callback) {
 /////展示全部合同重要信息
 exports.checkInfo = function(callback) {
 	Contract.find({}, {
+		cid: 1,
 		id: 1,
 		businessName: 1,
 		beginDate: 1,
@@ -79,11 +96,13 @@ exports.checkInfo = function(callback) {
 /////根据指定id展示合同详细信息
 exports.checkIdData = function(id, callback) {
 	Contract.find(id, function(err, docs) {
+//		console.log(docs);
 		callback(docs);
 		db.close();
 	});
 };
 ////修改事件完成标志位,随同会改变状态
+//参数
 exports.updateSymble = function(id, eventId, callback) {
 	Contract.update({
 		id: "CA123",
@@ -100,5 +119,30 @@ exports.updateSymble = function(id, eventId, callback) {
 			callback(docs);
 			db.close();
 		});
+	});
+};
+///////根据传入id重新编辑合同
+//参数:合同id,编辑后的合同json对象
+exports.updateIdData = function(id, result, callback) {
+	Contract.update({
+		cid: id
+	}, result, function() {
+		Contract.find({
+			cid: id
+		}, function(err, docs) {
+			callback(docs);
+			db.close();
+		});
+	});
+};
+///////计算一个合同已完成的事件个数,
+exports.countComEvent = function(id, callback) {
+	Contract.find(id, function(err, docus) {
+		var count = 0;
+		for (var i = 0; i < docus.events.length; i++) {
+			if (docus.events[i].completed === true)
+				count = count + 1;
+		}
+		callback(count);
 	});
 };
--- routes/contract.js
+++ routes/contract.js
@@ -23,172 +23,89 @@ var newCollection = db.collection("users3");
 //////////////test code//////////////
 exports.index = function(req, res) {
   /**************连接数据库操作********/
-  userModel.connectDb();
-//  userModel.insertData(contractData2);
-userModel.updateSymble('CA123','二期收款',function(data){
+  userModel.connectDb(); //XXXXXXXXXX一定得要开启数据库
+  /*********************************
+    userModel.insertData(contractData1);
+ /***********************************/
+  /************************************
+userModel.updateSymble("CA123","合同结束",function(data){
     res.send(data);
   });
-/***********************************
- userModel.checkInfo(function(data){
+/*************************************/
+  /**************************************
+userModel.updateData(function(data){
+   res.send(data);
+});
+/*************************************/
+  /***********************************
+  userModel.checkInfo(function(data) {
+    console.log('hello');
     res.send(data);
   });
   /**********************************/
 };
-exports.index1 = function(req, res) {
-  var getTime;
-  var getCondition;
-  var info;
-  var db=dataModel.connect();
-  var newCollection=dataModel.collection();
-  /******************修改完成标志位测试***********************
-  var id = {
-    Id: "CA123"
-  };
-  var option = {
-    Id: "CA123",
-    "events.name":"合同结束"
-    ///不要用完全事件键值来匹配,这样的话会导致事件必须得完整拿出来,不然会找不到.
-    //events: {name: "收取尾款",date: "2013-12-20",complete: false}
-  };
-  var result = {
-    "events.$.complete": true
-  };
-  dataModel.updateSymble(id, option, result, function(data) {
-    res.send(data);
-  });
-  /************************pass*****************************/
-  /********************追踪合同状态测试**********************
-  dataModel.checkCondition({Id:"CA123"},function(data) {
-    res.send(data);
-  });
-  /************************pass*****************************/
-  /***********************展示详细合同测试************************
-  dataModel.showData(function(data) {
-    res.send(data);
-  });
-  /************************pass*****************************???????
-  db.open(function(req, res1) {
-    dataModel.showData(function(data) {
-      res.send(data);
-      data1=data;
-      console.log(data);
-      db.close();
-    });
-  });
-  /*******************跟踪开始结束日期测试**********************
-  dataModel.checkTimeOf({
-    Id: "CA123"
-  }, function(data) {
-    res.send(data);
-  });
-  /**************************pass****************************/
-  /************************查询合同测试************************
-  dataModel.checkData({
-    id: "123"
-  }, function(data) {
-    res.send(data);
-  });
-  /**************************pass***************************/
-  /************************删除合同测试***********************
-  dataModel.removeDoc({
-    Id: "CA123"
-  }, function(data) {
-    res.send(data);
-  });
-  /***************************pass***************************/
-  /************************添加合同测试************************
-  var rdata = get.returnData1();
-  dataModel.insertDoc(rdata, function(data) {
-    console.log("good1");
-  });
-
-  //    res.send(data);
-  /***************************pass***************************/
-  /************************添加事件测试************************
-  var id = {
-    Id: "CA123"
-  };
-  var events = {
-    events: {
-      "name": "一次竞标",
-      "date": "2013-6-20",
-      "complete": false
-    }
-  };
-  dataModel.addEvent(id, events, function(data) {
-    res.send(data);
-  });
-  /***************************pass***************************/
-  /************************删除事件测试************************
-  var id = {
-    Id: "CA123"
-  };
-  var events = {
-    events: {
-      "name": "一次竞标"
-      //      "date": "2013-6-20",
-      //      "complete": false
-    }
-  };
-  dataModel.deleteEvent(id, events, function(data) {
-    res.send(data);
-  });
-  /***************************pass***************************/
-  /************************计算合同数目测试********************
-  dataModel.countDoc(function(data) {
-    res.send(data + "");
-  });
-  /***************************pass***************************/
-  /************************查询合同名字测试********************
-  dataModel.checkName({id:"123"},function(data) {
-    res.send(data);
-  });
-  /***************************pass***************************/
-  /************************展示所有合同测试********************
-  dataModel.showAllInfo(function(data) {
-    res.send(data);
-  });
-  /***************************pass***************************/
-  /************************展示合同信息测试********************
-  dataModel.showInfo({
-    Id: "CA123"
-  }, function(data) {
-    res.send(data);
-  });
-  /***************************No pass***************************/
-};
 exports.show = function(req, res) {
   console.log("啊啊啊啊啊啊啊啊" + req.params['contract']);
-  dataModel.showData(function(data) {
+  console.log("show");
+  userModel.connectDb();
+  var getId = {
+    cid: req.params['contract']
+  };
+  console.log(getId);
+  userModel.checkIdData(getId, function(data) {
     res.send(data);
   });
 };
 exports.create = function(req, res) {
+  console.log("create");
   userModel.connectDb();
   var rdata = req.body;
+  console.log(rdata);
   userModel.insertData(rdata);
 };
 
 exports.update = function(req, res) {
-  var option = {
-    Id: "CA123",
-    events: {
-      name: "收取尾款",
-      date: "2013-11-20",
-      complete: false
-    }
+  console.log("update");
+  userModel.connectDb();
+  //    var getId = req.body.cid;
+  //    var getNew = req.body[0];
+  var getId = "12548956837657480000";
+  var getNew = {
+    "cid": "12548956837657480000",
+    "id": "CS033022112",
+    "businessName": "南航统一移动平台1111",
+    "state": "回款",
+    "beginDate": "2013-01-02",
+    "endDate": "2014-05-03",
+    "events": [{
+        "remark": "时间一时间一时间一11111",
+        "date": "2013-06-09",
+        "title": "时间一",
+        "id": "0"
+      }, {
+        "price": "888",
+        "remark": "时间2时间2时间2时间2",
+        "date": "2013-06-09",
+        "title": "时间2",
+        "id": "1"
+      }
+    ]
   };
-  var result = {
-    "events.$.complete": "2013-11-20"
-  };
-  dataModel.updateSymble(option, result, function(data) {
+  console.log(getId);
+  console.log(getNew);
+  userModel.updateIdData(getId, getNew, function(data) {
     res.send(data);
   });
+  /*
+  userModel.updateSymble(option, result, function(data) {
+    res.send(data);
+  });
+*/
 };
 
 exports.destroy = function(req, res) {
   dataModel.removeDoc({
-    Id: "CA123"
+    id: "CA123"
   }, function(data) {
     res.send(data);
   });
