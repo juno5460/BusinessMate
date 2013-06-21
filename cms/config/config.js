d45cebde830ba82859d598a990231748da5c9893
neekychan <827286446@qq.com>
2013-06-21 08:44:46 +0800

    修正Git造成的代码混乱

--- cms/config/config.js
+++ cms/config/config.js
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-define([], function() {
 
-	var server = "localhost:3000";
-
-	return {
-		Server: function(path){
-			return "http://" + server + "/" + path;
-		}
-	}
-=======
 define([], function() {
 
 	var server = "10.108.1.67:3000";
@@ -18,5 +8,5 @@ define([], function() {
 			return "http://" + server + "/" + path;
 		}
 	}
->>>>>>> d08139fb3bf03e503d143658aa425674d283a4c4
+
 });
