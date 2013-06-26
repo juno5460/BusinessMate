2fcbf51af777db0c32ce4883cea0b49367d8b800
neekychan <827286446@qq.com>
2013-06-26 21:16:30 +0800

    能加载模板列表

--- views/assets/scripts/addContract.js
+++ views/assets/scripts/addContract.js
@@ -81,7 +81,39 @@ $(function(){
 		scrollToBottom();
 	});
 
+	var templateTmp = "<li><a hre='#'>{{templateName}}</a></li>";
 
+	$("#templateBtn").click(function(){
+		$.get("http://10.108.1.65:3000/api/templates",function(data,status){
+			if(status == 'success') {
+				var $templates = $(data);
+				$templates.each(function(index,item){
+					var $cellHtlm = $(Mustache.to_html(templateTmp, {templateName:item.tName}));
+					$("#templateList").html('');
+					$("#templateList").append($cellHtlm);
+				});
+			}
+		});
+	});
+
+
+	//点击保存模板按钮时
+	$("#saveAsTemplateBtn").click(function(){
+		var item = buildModel();
+		item.tName = item.name;
+		$.ajax({
+			url: 'http://10.108.1.65:3000/api/templates',
+			type: 'POST',
+			data: item,
+			success: function(result) {
+				console.info(result);
+			},
+			error: function(result){
+				alert("保存合同模板失败！");
+			}
+		});
+	});
+	
 	//保存按钮点击时
 	$("#saveBtn").click(function(){
 		var item = buildModel();
