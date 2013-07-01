b6614c9d46482291715e85914a12b58a0d0b55e5
neekychan <827286446@qq.com>
2013-07-01 17:49:59 +0800

    修复查询时的一出bug

--- views/assets/scripts/contracts.js
+++ views/assets/scripts/contracts.js
@@ -22,10 +22,16 @@ $(function(){
 
 	var showList = function(data) {
 		console.info(data);
+
+		//判断data是单个元素还是多个元素，不然后面的遍历操作会将单个元素的对象所有属性遍历出来。
+		if(!isArray(data)) {
+			data = [data];
+		}
+
 		$('#contractsTbody').html("");
 		$.each(data,function(index,item){
 			var trTemplate = "<tr><td class='center'>{{myId}}</td><td class='center'>{{name}}</td><td class='center'>{{partyA}}</td><td class='center'>{{partyB}}</td><td class='center'>{{beginDate}}</td><td class='center'>{{endDate}}</td><td class='center'>{{signDate}}</td><td class='center'>{{amount}}</td><td class='center'>{{state}}</td></tr>";
-
+			console.info(index,item);
 			var $trHtlm = $(Mustache.to_html(trTemplate, item));
 			$trHtlm.click(function(){
 				console.info(item._id);
@@ -33,5 +39,9 @@ $(function(){
 			});
 			$('#contractsTbody').append($trHtlm);
 		});
+	}
+
+	var isArray = function(obj) {
+		return Object.prototype.toString.call(obj) === '[object Array]';
 	}
 });
