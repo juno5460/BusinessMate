154ec2126714624e87fd2da631d7610da7f8e13b
neekychan <827286446@qq.com>
2013-07-01 17:28:01 +0800

    增加基础搜索功能

--- views/assets/scripts/contracts.js
+++ views/assets/scripts/contracts.js
@@ -4,8 +4,25 @@ $(function(){
 		
 	});
 
+	$("#searchBtn").click(function(){
+
+		var $keyword = $("#keyword").val();
+
+		$.get('/api/contracts/' + $keyword,function(data, status){
+			$('#contractsTbody').html("");
+			showList(data);
+		});
+
+	});
+
 	$.get('/api/contracts',function(data, status){
+		showList(data);
+	});
+
+
+	var showList = function(data) {
 		console.info(data);
+		$('#contractsTbody').html("");
 		$.each(data,function(index,item){
 			var trTemplate = "<tr><td class='center'>{{myId}}</td><td class='center'>{{name}}</td><td class='center'>{{partyA}}</td><td class='center'>{{partyB}}</td><td class='center'>{{beginDate}}</td><td class='center'>{{endDate}}</td><td class='center'>{{signDate}}</td><td class='center'>{{amount}}</td><td class='center'>{{state}}</td></tr>";
 
@@ -16,5 +33,5 @@ $(function(){
 			});
 			$('#contractsTbody').append($trHtlm);
 		});
-	});
+	}
 });
