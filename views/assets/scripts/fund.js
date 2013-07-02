17965d07dc3e33ff8fb6a704f7e92403f5dc5fda
kekobin <kekobin@163.com>
2013-07-02 10:44:05 +0800

    回款饼图比例

--- views/assets/scripts/fund.js
+++ views/assets/scripts/fund.js
@@ -17,7 +17,7 @@ $(function() {
 					};
 
 					var curRatio1 = (t1data.returnRatio)*100;
-					var easypieRatio1 = "<div class='pull-middle easy-pie-chart1 percentage easyPieChart' data-size='30' data-color='#ECCB71' data-percent='42' style='width: 30px; height: 30px; line-height: 30px;'>"+
+					var easypieRatio1 = "<div class='pull-middle easy-pie-chart1 percentage easyPieChart' data-size='40' data-color='#ECCB71' data-percent='"+curRatio1+"' style='width: 30px; height: 30px; line-height: 30px;'>"+
 																	"	<span class='percent'>"+curRatio1+"</span>%<canvas width='30' height='30'></canvas></div>";
 					var template1 = "<tr><td class='center'>{{name}}</td><td class='hidden-480 center'>{{partyA}}</td><td class='hidden-480 center'>{{partyB}}</td><td  class='center'>{{amount}}</td><td class='center'>{{returnAmount}}</td><td class='hidden-phone center'>"+easypieRatio1+"</td><td class='hidden-480 center'>{{lastReturnDate}}</td></tr>";
 
@@ -53,7 +53,7 @@ $(function() {
 					};
 
 					var curRatio2 = (t2data.returnRatio)*100;
-					var easypieRatio2 = "<div class='pull-middle easy-pie-chart2 percentage easyPieChart' data-size='40' data-color='#ECCB71' data-percent='42' style='width: 30px; height: 30px; line-height: 30px;'>"+
+					var easypieRatio2 = "<div class='pull-middle easy-pie-chart2 percentage easyPieChart' data-size='42' data-color='#ECCB71' data-percent='"+curRatio2+"' style='width: 30px; height: 30px; line-height: 30px;'>"+
 																	"	<span class='percent'>"+curRatio2+"</span>%<canvas width='30' height='30'></canvas></div>";
 					var template2 = "<tr><td class='center'>{{name}}</td><td class='hidden-480 center'>{{partyA}}</td><td class='hidden-480 center'>{{partyB}}</td><td class='center'>{{amount}}</td><td  class='center'>{{returnAmount}}</td><td class='hidden-phone center'>"+easypieRatio2+"</td><td class='hidden-phone center'>{{lastReturnDate}}</td><td  class='center'>{{remark}}</td></tr>";
 
@@ -86,7 +86,8 @@ $(function() {
 					};
 
 					var curRatio3 = (t3data.returnRatio)*100;
-					var easypieRatio3 = "<div class='pull-middle easy-pie-chart3 percentage easyPieChart' data-size='30' data-color='#ECCB71' data-percent='42' style='width: 30px; height: 30px; line-height: 30px;'>"+
+					curRatio3=0;
+					var easypieRatio3 = "<div class='pull-middle easy-pie-chart3 percentage easyPieChart' data-size='30' data-color='#ECCB71' data-percent='"+curRatio3+"' style='width: 30px; height: 30px; line-height: 30px;'>"+
 																	"	<span class='percent'>"+curRatio3+"</span>%<canvas width='30' height='30'></canvas></div>";
 					var template3 = "<tr><td  class='center'>{{name}}</td><td class='hidden-480 center'>{{partyA}}</td><td class='hidden-480 center'>{{partyB}}</td><td class='center'>{{amount}}</td><td class='center'>{{returnAmount}}</td><td class='hidden-phone center'>"+easypieRatio3+"</td><td  class='center'>{{lastReturnDate}}</td></tr>";
 
