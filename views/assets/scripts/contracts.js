2c8d915c8e6cfbf3c48312fe1bd7d277d679a0bd
neekychan <827286446@qq.com>
2013-07-03 09:04:35 +0800

    增加高级搜索菜单。

--- views/assets/scripts/contracts.js
+++ views/assets/scripts/contracts.js
@@ -1,5 +1,16 @@
 $(function(){
 
+	$('#beginDate').datepicker();
+	$('#beginDate').datepicker().on('changeDate', function(env) {
+		$('#beginDate').datepicker('hide');
+	});
+	$('#endDate').datepicker();
+	$('#endDate').datepicker().on('changeDate', function(env) {
+		$('#endDate').datepicker('hide');
+	});
+
+	var isDropDown = false;
+
 	$("#addContractBtn").click(function(){
 		
 	});
@@ -14,18 +25,66 @@ $(function(){
 		onSearch();
 	});
 
+	$("#search_btn").click(function(){
+		onCustomSearch();
+	});
+
+
+	$("#drowdown").click(function(){
+		if(!isDropDown){
+			isDropDown = true;
+			$("#customSearchPanel").css("display","block");
+		} else {
+			isDropDown = false;
+			$("#customSearchPanel").css("display","none");
+		}
+		
+	});
+
+	$("#removeBtn").click(function(){
+		$("#customSearchPanel").css("display","none");
+		isDropDown = false;
+	});
+
 	var onSearch = function(){
+
 		var $keyword = $("#keyword").val();
 
-		$.get('/api/contracts/' + $keyword,function(data, status){
+		var $postJson  			= {};
+		$postJson.id 			= true;
+		$postJson.name 			= true;
+		$postJson.remark 		= true;
+		$postJson.keyword   	= $keyword;
+		$postJson.beginDate  	= '';
+		$postJson.endDate 		= '';
+
+		$.get('/api/contracts/' + JSON.stringify($postJson),function(data, status){
 			$('#contractsTbody').html("");
 			showList(data);
 		});
 	}
 
-	$.get('/api/contracts',function(data, status){
-		showList(data);
-	});
+	var onCustomSearch = function(){
+
+		var checkId  	= $("#search_check_id").is(':checked');
+		var checkName 	= $("#search_check_name").is(':checked');
+		var checkRemark = $("#search_check_remark").is(':checked');
+
+		var $postJson  			= {};
+		$postJson.id 			= checkId;
+		$postJson.name 			= checkName;
+		$postJson.remark 		= checkRemark;
+		$postJson.keyword   	= $("#search_keyword").val();
+		$postJson.beginDate  	= $("#beginDate").val();
+		$postJson.endDate 		= $("#endDate").val();
+
+		console.info(JSON.stringify($postJson));
+
+		$.get('/api/contracts/' + JSON.stringify($postJson), function(data, status) {
+			$('#contractsTbody').html("");
+			showList(data);
+		});
+	}
 
 
 	var showList = function(data) {
@@ -47,7 +106,7 @@ $(function(){
 
 		$('#contractsTbody').html("");
 		$.each(data,function(index,item){
-			var trTemplate = "<tr><td class='center'>{{myId}}</td><td class='center'>{{name}}</td><td class='center'>{{partyA}}</td><td class='center'>{{partyB}}</td><td class='center'>{{beginDate}}</td><td class='center'>{{endDate}}</td><td class='center'>{{signDate}}</td><td class='center'>{{amount}}</td><td class='center'>{{state}}</td></tr>";
+			var trTemplate = "<tr><td class='center hidden-480'>{{myId}}</td><td class='center'>{{name}}</td><td class='center'>{{partyA}}</td><td class='center'>{{partyB}}</td><td class='center'>{{beginDate}}</td><td class='center'>{{endDate}}</td><td class='center hidden-480'>{{signDate}}</td><td class='center'>{{amount}}</td><td class='center'>{{state}}</td></tr>";
 			console.info(index,item);
 			var $trHtlm = $(Mustache.to_html(trTemplate, item));
 			$trHtlm.click(function(){
