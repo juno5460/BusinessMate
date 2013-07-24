1385b549e37eb1d543471ac16280210406d4a7f4
kekobin <kekobin@163.com>
2013-07-24 09:20:09 +0800

    update

--- views/assets/scripts/desktop.js
+++ views/assets/scripts/desktop.js
@@ -20,7 +20,7 @@ $(function() {
 
 	function stringToHex(str) {
 		var val = "";
-		for (var i = 0; i < str.length; i++) {
+		for (var i in str) {
 			if (val == "")
 				val = str.charCodeAt(i).toString(16);
 			else
