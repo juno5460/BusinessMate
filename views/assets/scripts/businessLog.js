b9ca25478c96d3c7bf6403874e43f83cb21868f0
kekobin <kekobin@163.com>
2013-07-30 10:30:04 +0800

    update

--- views/assets/scripts/businessLog.js
+++ views/assets/scripts/businessLog.js
@@ -3,8 +3,8 @@ $(function() {
 		$.each(data, function(i, log) {
 		console.info(log);
 			var cname = log.contractName;
-			// if(cname.length > 20)
-			// 	cname = cname.substring(0, 19) + "...";
+			if(cname.length > 10)
+				cname = cname.substring(0, 9) + "...";
 			var tdata = {
 				time: log.time,
 				cname: cname,
@@ -14,7 +14,7 @@ $(function() {
 				state: log.data.state
 			};
 			console.info(tdata.state);                                                                                                                                                   
-			var template = "<div id='blog' class='span12'><div id='box' class='widget-box collapsed'><div class='widget-header widget-header-small'><h6><table><tr><th style=width:380px>{{time}}</th><th style=width:140px title="+log.contractName+">{{cname}}</th><th style=width:80px>版本:{{version}}</th><th style=width:60px>{{deptA}}</th><th style=width:60px>{{deptB}}</th><th style=width:50px>{{state}}</th></tr></table></h6><div class='widget-toolbar'><a href='#' data-action='collapse'> <i id='iClick' class='icon-chevron-down'></i></a></div></div><div class='widget-body'><div class='widget-body-inner' style='display: block;'><div class='widget-main'><table style='width:100%'><thead><tr><th class='center'>事件名称</th><th class='center'>事件类型</th><th class='center'>发票日期</th><th class='center'>执行时间</th><th class='center'>回款金额</th></tr></thead><tbody id='content'></tbody></table></div></div></div></div></div>";
+			var template = "<div id='blog' class='span12'><div id='box' class='widget-box collapsed'><div class='widget-header widget-header-small'><h6><table><tr><th style=width:380px>{{time}}</th><th style=width:140px title="+log.contractName+">{{cname}}</th><th style=width:80px>版本:{{version}}</th><th style=width:60px>{{deptA}}</th><th style=width:60px>{{deptB}}</th><th style=width:140px>{{state}}</th></tr></table></h6><div class='widget-toolbar'><a href='#' data-action='collapse'> <i id='iClick' class='icon-chevron-down'></i></a></div></div><div class='widget-body'><div class='widget-body-inner' style='display: block;'><div class='widget-main'><table style='width:100%'><thead><tr><th class='center'>事件名称</th><th class='center'>事件类型</th><th class='center'>发票日期</th><th class='center'>执行时间</th><th class='center'>回款金额</th></tr></thead><tbody id='content'></tbody></table></div></div></div></div></div>";
 			var $templateHtml = $(Mustache.to_html(template, tdata));
 
 			$("#businesslog").append($templateHtml);
--- views/assets/scripts/userLog.js
+++ views/assets/scripts/userLog.js
@@ -2,9 +2,9 @@ $(function() {
 	$.get('/api/userlog', function(data, status) {
 		$.each(data, function(i, log) {
 			var cname = log.data.name;
-			console.info(cname);
-			// if(cname.length > 20)
-			// 	cname = cname.substring(0, 19) + "...";
+
+			if(cname.length > 10)
+				cname = cname.substring(0, 9) + "...";
 			var tdata = {
 				time: log.time,
 				user : log.user,
@@ -16,7 +16,7 @@ $(function() {
 				state: log.data.state
 			};
 
-			var template = "<div id='ublog' class='span12'><div id='box' class='widget-box collapsed'><div class='widget-header widget-header-small'><h6><table><tr><th style=width:380px>{{time}}</th><th style=width:50px>{{user}}</th><th style=width:140px>{{cname}}</th><th style=width:50px>{{operation}}</th><th style=width:50px>{{resource}}</th><th style=width:50px>{{deptA}}</th><th style=width:50px>{{deptB}}</th><th style=width:50px>{{state}}</th></tr></table></h6><div class='widget-toolbar'><a href='#' data-action='collapse'> <i id='iClick' class='icon-chevron-down'></i></a></div></div><div class='widget-body'><div class='widget-body-inner' style='display: block;'><div class='widget-main'><table style='width:100%'><thead><tr><th class='center'>事件名称</th><th class='center'>事件类型</th><th class='center'>发票日期</th><th class='center'>执行时间</th><th class='center'>回款金额</th></tr></thead><tbody id='tcontent'></tbody></table></div></div></div></div></div>";
+			var template = "<div id='ublog' class='span12'><div id='box' class='widget-box collapsed'><div class='widget-header widget-header-small'><h6><table><tr><th style=width:380px>{{time}}</th><th style=width:50px>{{user}}</th><th style=width:140px>{{cname}}</th><th style=width:50px>{{operation}}</th><th style=width:50px>{{resource}}</th><th style=width:50px>{{deptA}}</th><th style=width:50px>{{deptB}}</th><th style=width:140px>{{state}}</th></tr></table></h6><div class='widget-toolbar'><a href='#' data-action='collapse'> <i id='iClick' class='icon-chevron-down'></i></a></div></div><div class='widget-body'><div class='widget-body-inner' style='display: block;'><div class='widget-main'><table style='width:100%'><thead><tr><th class='center'>事件名称</th><th class='center'>事件类型</th><th class='center'>发票日期</th><th class='center'>执行时间</th><th class='center'>回款金额</th></tr></thead><tbody id='tcontent'></tbody></table></div></div></div></div></div>";
 			var $templateHtml = $(Mustache.to_html(template, tdata));
 
 			$("#userlog").append($templateHtml);
